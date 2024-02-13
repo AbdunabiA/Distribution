68b7d3bfa19678fb04201156b26164b4aa281b86
AbdunabiA <abdunabiabduvahobov86@gmail.com>
2024-02-13 16:07:04 +0500

    svg import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import jsconfigPaths from "vite-jsconfig-paths";
+import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), jsconfigPaths()],
+  plugins: [react(), jsconfigPaths(), svgr()],
 });
