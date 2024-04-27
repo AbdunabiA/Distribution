60b60e613b7f5b2c676821f1f052cebd7711049d
AbdunabiA <abdunabiabduvahobov86@gmail.com>
2024-04-27 16:17:56 +0500

    forms are added

--- src/components/inputs/index.jsx
+++ src/components/inputs/index.jsx
@@ -1,3 +1,4 @@
 export {CustomInput} from './customInput'
 export {AsyncSelect} from './asyncSelect'
 export {CustomTextArea} from './customTextArea'
+export { CustomSelect } from './customSelect';
---
+++ src/pages/manager/products/product/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ManagerProducts = () => {
+  return (
+    <div>ManagerProducts</div>
+  )
+}
+
+export default ManagerProducts
