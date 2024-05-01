69a34120a336b5496656d345733776837a0bbe32
AbdunabiA <abdunabiabduvahobov86@gmail.com>
2024-05-01 19:22:43 +0500

    productsendform

--- src/components/forms/createProductCategory/index.jsx
+++ src/components/forms/createProductCategory/index.jsx
@@ -1,3 +1,4 @@
+import { useQueryClient } from "@tanstack/react-query";
 import { Button } from "antd";
 import { CustomInput } from "components/inputs";
 import { get } from "lodash";
@@ -5,6 +6,7 @@ import { ContainerForm } from "modules";
 import { toast } from "sonner";
 
 export const CreateProductCategory = ({data, setModal}) => {
+  const queryClient = useQueryClient()
   return (
     <ContainerForm
       fields={[
@@ -18,13 +20,16 @@ export const CreateProductCategory = ({data, setModal}) => {
           value: get(data, "status", "active"),
         },
       ]}
-      method="post"
-      url="/categories/"
+      method={data ? "put" : "post"}
+      url={data ? `/category/${data.id}/` : "/categories/"}
       onSuccess={() => {
+        queryClient.invalidateQueries("/categories/");
         setModal({ isOpen: false, form: null, data: null });
-        toast.success("Kategoriya yaratildi");
+        toast.success(`Kategoriya ${data ? "o'zgartirildi" : "yaratildi"}`);
       }}
-      onError={() => toast.error("Kategoriya yaratilmadi")}
+      onError={() =>
+        toast.error(`Kategoriya ${data ? "o'zgartirilmadi" : "yaratilmadi"}`)
+      }
     >
       {({ handleSubmit, isLoading }) => {
         return (
@@ -36,9 +41,7 @@ export const CreateProductCategory = ({data, setModal}) => {
               gap: "20px",
             }}
           >
-            <h2>
-              {data ? "Kategoriya o'zgartirish" : "Kategoriya yaratish"}
-            </h2>
+            <h2>{data ? "Kategoriya o'zgartirish" : "Kategoriya yaratish"}</h2>
             <CustomInput placeholder="kategoriya nomi" name="name" />
             <div style={{ display: "flex", justifyContent: "end" }}>
               <Button
