5fea721063025985c14ab26da1a1dc34e23a034b
AbdunabiA <abdunabiabduvahobov86@gmail.com>
2024-05-06 19:07:16 +0500

    task create form corrected

--- src/components/forms/createTask/index.jsx
+++ src/components/forms/createTask/index.jsx
@@ -61,11 +61,11 @@ export const CreateTask = ({ data, setModal }) => {
         },
         {
           name: "task_setter",
-          value: "",
+          value: userData.id,
         },
         {
           name: "role",
-          value:null,
+          value: null,
           disabled: true,
         },
         {
@@ -76,12 +76,12 @@ export const CreateTask = ({ data, setModal }) => {
         },
       ]}
       onSuccess={() => {
-        setModal({isOpen:false, data:null})
-        toast.success("Topshiriq yaratildi")
+        setModal({ isOpen: false, data: null });
+        toast.success("Topshiriq yaratildi");
       }}
       onError={(err) => toast.error(err.message)}
     >
-      {({handleSubmit, isLoading, values}) => {
+      {({ handleSubmit, isLoading, values }) => {
         console.log(values?.role);
         return (
           <div
